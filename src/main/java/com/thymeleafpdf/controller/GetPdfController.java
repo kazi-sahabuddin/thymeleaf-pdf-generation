eb0765adc19f0a34b4d859058797f82efc384e39
kazi-sahabuddin <kazi.sahabuddin16@gmail.com>
2024-04-19 17:10:35 +0600

    Pdf file created

--- src/main/java/com/thymeleafpdf/controller/GetPdfController.java
+++ src/main/java/com/thymeleafpdf/controller/GetPdfController.java
@@ -1,10 +1,23 @@
 package com.thymeleafpdf.controller;
 
+import com.thymeleafpdf.service.CreatePdfFileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GetPdfController {
+    @Autowired
+    private CreatePdfFileService createPdfFileService;
+
+    @GetMapping("pdf-file")
     public String getPdf(){
         return "getPdfFile";
+    }
+
+    @GetMapping("/create-pdf")
+    public String pdfFile(){
+        createPdfFileService.createPdf();
+        return "redirect:pdf-file";
     }
 }
--- src/main/java/com/thymeleafpdf/service/CreatePdfFileService.java
+++ src/main/java/com/thymeleafpdf/service/CreatePdfFileService.java
@@ -3,6 +3,7 @@ package com.thymeleafpdf.service;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
 import org.springframework.stereotype.Service;
 
 import java.io.FileNotFoundException;
@@ -11,11 +12,13 @@ import java.io.FileNotFoundException;
 public class CreatePdfFileService {
 
     public void createPdf(){
-        String filePdfPath = "/sample.pdf";
+        String filePdfPath = "/home/sahabuddin/IdeaProjects/sample.pdf";
         try{
             PdfWriter writer = new PdfWriter(filePdfPath);
             PdfDocument pdfDco = new PdfDocument(writer);
             Document doc = new Document(pdfDco);
+            Paragraph p1 = new Paragraph("Hello, welcome to goja munna");
+            doc.add(p1);
             doc.close();
         } catch (FileNotFoundException exception){
             exception.printStackTrace();
